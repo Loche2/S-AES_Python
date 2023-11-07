7dce2af8d7eb4d1938667d4709880d0678894e16
MaxAndFelix <MaxAndFelixZheng@outlook.com>
2023-11-08 00:48:18 +0800

    Support muliple pairs of cypertext and plaintext

--- brute_force.py
+++ brute_force.py
@@ -3,25 +3,42 @@ class meet_in_the_middle_attack():
     def __init__(self,plaintext,cypertext):
         self.plaintext = plaintext
         self.cypertext = cypertext
-        self.pair = []
+        self.pairs = []
     def brute_force(self):
         """
             暴力破解
-        """   
+        """  
+        plain=self.plaintext[0]
+        cyper=self.cypertext[0]
         ans=[]
         for i in range(2**16):
             binary_string_i = format(i, '016b')
             S1 = S_AES.S_AES(binary_string_i)
-            middle_text = S1.encrypt(self.plaintext)
+            middle_text = S1.encrypt(plain)
             ans.append([int(middle_text, 2),i])#middletext(int) i->K1
         sorted_ans = sorted(ans, key=lambda x: x[0])    
-        print(sorted_ans)       
+        # print(sorted_ans)       
         for j in range(2**16):
             binary_string_j = format(j, '016b')
             S2 = S_AES.S_AES(binary_string_j)
-            middle_text = S2.decrypt(self.cypertext)
+            middle_text = S2.decrypt(cyper)
             self.binary_search(sorted_ans,[int(middle_text, 2),j])
-        return self.pair
+        for i in range(1,len(self.plaintext)):
+            copy = []
+            print(self.plaintext[i],self.cypertext[i])
+            for j,pair in enumerate(self.pairs):
+                S1 = S_AES.S_AES(pair[0])
+                S2 = S_AES.S_AES(pair[1])
+                if j == len(self.pairs)-1:
+                    print("ok")
+                if S1.encrypt(self.plaintext[i])==S2.decrypt(self.cypertext[i]):
+                    copy.append(self.pairs[i])
+                    print(S1.encrypt(self.plaintext[i]),S2.decrypt(self.cypertext[i]))
+                    continue
+                else:
+                    continue
+            self.pairs = copy
+        return self.pairs
     def binary_search(self, sorted_ans, key):
             l = 0
             r = len(sorted_ans) - 1
@@ -34,7 +51,7 @@ class meet_in_the_middle_attack():
             if sorted_ans[l][0] == key[0]:
                 # print(sorted_ans[l][0],key[0])
                 # print([format(sorted_ans[l][1], '016b'),format(key[1], '016b')])
-                self.pair.append([format(sorted_ans[l][1], '016b'),format(key[1], '016b')])
+                self.pairs.append([format(sorted_ans[l][1], '016b'),format(key[1], '016b')])
             else:
                 return 0
             while(1):
@@ -44,7 +61,7 @@ class meet_in_the_middle_attack():
                 if sorted_ans[l][0]== key[0]:
                     # print(sorted_ans[l][0],key[0])
                     # print([format(sorted_ans[l][1], '016b'),format(key[1], '016b')])
-                    self.pair.append([format(sorted_ans[l][1], '016b'),format(key[1], '016b')])
+                    self.pairs.append([format(sorted_ans[l][1], '016b'),format(key[1], '016b')])
                 else:
                     break
             while(1):
@@ -54,17 +71,12 @@ class meet_in_the_middle_attack():
                 if sorted_ans[r][0]== key[0]:
                     # print(sorted_ans[r][0],key[0])
                     # print([format(sorted_ans[r][1], '016b'),format(key[1], '016b')])
-                    self.pair.append([format(sorted_ans[r][1], '016b'),format(key[1], '016b')])
+                    self.pairs.append([format(sorted_ans[r][1], '016b'),format(key[1], '016b')])
                 else:
                     break
-    def bf(self):
-        mid_texts = []
-        find_keys = []
-        for key in range(2**16):
-            mid_texts.append()
 
 if __name__=='__main__':
-    plain_text = '1010101010101010'
-    cyper_text = '1111111111111111'
+    plain_text = ['1010101010101010','1111000011110000']
+    cyper_text = ['1111111111111111','0101101001011010']
     M=meet_in_the_middle_attack(plain_text,cyper_text)
-    print(len(M.brute_force()))
+    print(M.brute_force())
